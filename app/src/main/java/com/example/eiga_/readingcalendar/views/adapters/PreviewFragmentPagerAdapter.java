c15338ab6a539d7d011e4535f707261f119d7492
KeitaKato <amx37zhuravlik@gmail.com>
2018-08-17 01:54:08 +0900

    tess-two test

--- app/src/main/java/com/example/eiga_/readingcalendar/views/adapters/PreviewFragmentPagerAdapter.java
+++ app/src/main/java/com/example/eiga_/readingcalendar/views/adapters/PreviewFragmentPagerAdapter.java
@@ -1,5 +1,8 @@
 package com.example.eiga_.readingcalendar.views.adapters;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,13 +11,18 @@ import android.support.v4.app.FragmentPagerAdapter;
 import com.example.eiga_.readingcalendar.fragments.CameraPreviewFragment;
 import com.example.eiga_.readingcalendar.fragments.OCRPreviewFragment;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
 public class PreviewFragmentPagerAdapter extends FragmentPagerAdapter {
 
     private String imageUriString;
+    private String OCRData;
 
     public PreviewFragmentPagerAdapter(FragmentManager manager, String imageUriString) {
         super(manager);
         this.imageUriString = imageUriString;
+        this.OCRData = getOCRData(imageUriString);
     }
 
     @Override
@@ -44,5 +52,11 @@ public class PreviewFragmentPagerAdapter extends FragmentPagerAdapter {
 
         }
         return null;
+    }
+
+    private String getOCRData(String imageUriString) {
+        // UriをInputStreamを生成。
+        InputStream stream = context.getContentResolver().openInputStream(Uri.parse(imageUriString));
+        Bitmap bitmap = BitmapFactory.decodeStream(new BufferedInputStream(stream));
     }
 }
